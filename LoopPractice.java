36192e78c1f2166846413ebc4b4c0416e611d120
Varun Chadha <vchadha99@gmail.com>
2025-06-10 20:55:41 +0530

    Add files via upload

---
+++ LoopPractice.java
@@ -0,0 +1,18 @@
+package PracticePack;
+
+public class LoopPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=100;i++) {
+			if((i%5)!=0) {
+				System.out.print(i +" ");
+			}
+			else {
+				System.out.print("* ");
+			}
+		}
+
+	}
+
+}
